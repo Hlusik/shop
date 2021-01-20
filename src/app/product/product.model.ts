2b9a4d7d995b2a7367489ce3e1c327aa26e6ae20
Halyna_Mykolaiv <Halyna_Mykolaiv@epam.com>
2021-01-20 18:26:13 +0200

    first task

---
+++ src/app/product/product.model.ts
@@ -0,0 +1,11 @@
+import { Category } from "../first/category.enum";
+
+export class Product {
+    constructor(
+      public name: string,
+      public description: string,
+      public price: number,
+      public category: Category,
+      public isAvailable: boolean,
+    ) {}
+  }
