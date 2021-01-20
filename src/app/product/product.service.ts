f5d208d986d50a84251e414ac10039ed65b8fe64
Halyna_Mykolaiv <Halyna_Mykolaiv@epam.com>
2021-01-20 18:26:13 +0200

    first task

---
+++ src/app/product/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Category } from '../first/category.enum';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  getProducts(): Product[]{
+    return [
+      new Product('Banana', 'yellow and sweet', 10, Category.Left, true),
+      new Product('Apple', 'red and sweet', 20, Category.Down, true),
+      new Product('Lime', 'green and sour', 15, Category.Right, false),
+    ];
+  }
+}
